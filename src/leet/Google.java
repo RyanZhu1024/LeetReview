b19d5a8eadb61d46bbb6d68f02ad7a4fcc35570f
Ryan Zhu <rzhu@financialengines.com>
2016-08-11 10:01:32 -0700

    add int to roman, o1

--- src/leet/Google.java
+++ src/leet/Google.java
@@ -11,17 +11,36 @@ import java.util.List;
 public class Google {
     public static void main(String[] args) {
         Google g = new Google();
-        System.out.println(g.solution(
-                "1.png\n" +
-                "dir1\n" +
-                " dir11\n" +
-                " dir12\n" +
-                "  picture.jpeg\n" +
-                "  dir121\n" +
-                "   2.gif\n" +
-                "  file1.txt\n" +
-                "dir2\n" +
-                " file2.gif"));
+        System.out.println(g.isMatch("aa", ".*"));
+    }
+
+    public boolean isMatch(String s, String p) {
+        // write your code here
+        if (s == null || p == null) {
+            return false;
+        } else {
+            int m = s.length(), n = p.length();
+            boolean[][] dp = new boolean[m + 1][n + 1];
+            dp[0][0] = true;
+            for (int i = 1; i < m + 1; i++) {
+                dp[i][0] = false;
+            }
+            for (int i = 1; i < n + 1; i++) {
+                if (p.charAt(i - 1) == '*') {
+                    dp[0][i] = dp[0][i - 2];
+                }
+            }
+            for (int i = 1; i < m + 1; i++) {
+                for (int j = 1; j < n + 1; j++) {
+                    if (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '.') {
+                        dp[i][j] = dp[i - 1][j - 1];
+                    } else if (p.charAt(j - 1) == '*') {
+                        dp[i][j] = (dp[i - 1][j - 1] && s.charAt(i - 1) == p.charAt(j - 2) || p.charAt(j - 2) == '.') || dp[i][j - 1] || dp[i][j - 2];
+                    }
+                }
+            }
+            return dp[m][n];
+        }
     }
     public int[] maxSlidingWindow(int[] nums, int k) {
         if (nums == null || nums.length == 0) {
